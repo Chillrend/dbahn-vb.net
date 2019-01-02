fbc3fcb1718d2f3f73fd11dc6972f5c2b174050c
Farhan Hanif <cv2lex@gmail.com>
2019-01-02 10:54:24 +0700

    Revised CRUD Modules, Added two tables for operation.

--- DeutscheBahn/crud.Designer.vb
+++ DeutscheBahn/crud.Designer.vb
@@ -39,9 +39,36 @@ Partial Class crud
         Me.Label3 = New System.Windows.Forms.Label()
         Me.stationTable = New System.Windows.Forms.DataGridView()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.seat_id = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.identification = New System.Windows.Forms.TextBox()
+        Me.passgr_name = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.tickets = New System.Windows.Forms.TextBox()
+        Me.tkt_id = New System.Windows.Forms.Label()
+        Me.invo_id = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.inv_table = New System.Windows.Forms.DataGridView()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.btn_add = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_edit = New System.Windows.Forms.Button()
+        Me.name_box = New System.Windows.Forms.TextBox()
+        Me.Train = New System.Windows.Forms.Label()
+        Me.train_id_box = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.tbl_train = New System.Windows.Forms.DataGridView()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         CType(Me.stationTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.inv_table, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.tbl_train, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -68,6 +95,7 @@ Partial Class crud
         '
         Me.TabControl1.Controls.Add(Me.TabPage1)
         Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
         Me.TabControl1.Location = New System.Drawing.Point(33, 76)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
@@ -204,13 +232,238 @@ Partial Class crud
         '
         'TabPage2
         '
+        Me.TabPage2.Controls.Add(Me.Label9)
+        Me.TabPage2.Controls.Add(Me.seat_id)
+        Me.TabPage2.Controls.Add(Me.Button2)
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.identification)
+        Me.TabPage2.Controls.Add(Me.passgr_name)
+        Me.TabPage2.Controls.Add(Me.Label8)
+        Me.TabPage2.Controls.Add(Me.tickets)
+        Me.TabPage2.Controls.Add(Me.tkt_id)
+        Me.TabPage2.Controls.Add(Me.invo_id)
+        Me.TabPage2.Controls.Add(Me.Label10)
+        Me.TabPage2.Controls.Add(Me.inv_table)
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
         Me.TabPage2.Size = New System.Drawing.Size(679, 391)
         Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.Text = "Invoice"
         Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(450, 23)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 15)
+        Me.Label9.TabIndex = 25
+        Me.Label9.Text = "Seat ID"
+        '
+        'seat_id
+        '
+        Me.seat_id.Location = New System.Drawing.Point(535, 20)
+        Me.seat_id.Name = "seat_id"
+        Me.seat_id.Size = New System.Drawing.Size(111, 20)
+        Me.seat_id.TabIndex = 24
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Roboto Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(558, 281)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 57)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "ADD"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Roboto Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(558, 218)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 57)
+        Me.Button3.TabIndex = 22
+        Me.Button3.Text = "DELETE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(558, 155)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 57)
+        Me.Button4.TabIndex = 21
+        Me.Button4.Text = "EDIT"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(235, 69)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(80, 15)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Identification"
+        '
+        'identification
+        '
+        Me.identification.Location = New System.Drawing.Point(320, 66)
+        Me.identification.Name = "identification"
+        Me.identification.Size = New System.Drawing.Size(111, 20)
+        Me.identification.TabIndex = 19
+        '
+        'passgr_name
+        '
+        Me.passgr_name.Location = New System.Drawing.Point(320, 20)
+        Me.passgr_name.Name = "passgr_name"
+        Me.passgr_name.Size = New System.Drawing.Size(111, 20)
+        Me.passgr_name.TabIndex = 18
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(251, 25)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(42, 15)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Name"
+        '
+        'tickets
+        '
+        Me.tickets.Location = New System.Drawing.Point(110, 67)
+        Me.tickets.Name = "tickets"
+        Me.tickets.Size = New System.Drawing.Size(111, 20)
+        Me.tickets.TabIndex = 16
+        '
+        'tkt_id
+        '
+        Me.tkt_id.AutoSize = True
+        Me.tkt_id.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tkt_id.Location = New System.Drawing.Point(46, 68)
+        Me.tkt_id.Name = "tkt_id"
+        Me.tkt_id.Size = New System.Drawing.Size(57, 15)
+        Me.tkt_id.TabIndex = 15
+        Me.tkt_id.Text = "Ticket ID"
+        '
+        'invo_id
+        '
+        Me.invo_id.Location = New System.Drawing.Point(110, 20)
+        Me.invo_id.Name = "invo_id"
+        Me.invo_id.Size = New System.Drawing.Size(111, 20)
+        Me.invo_id.TabIndex = 14
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(46, 22)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(19, 15)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "ID"
+        '
+        'inv_table
+        '
+        Me.inv_table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.inv_table.Location = New System.Drawing.Point(49, 131)
+        Me.inv_table.Name = "inv_table"
+        Me.inv_table.Size = New System.Drawing.Size(464, 239)
+        Me.inv_table.TabIndex = 12
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.btn_add)
+        Me.TabPage3.Controls.Add(Me.btn_delete)
+        Me.TabPage3.Controls.Add(Me.btn_edit)
+        Me.TabPage3.Controls.Add(Me.name_box)
+        Me.TabPage3.Controls.Add(Me.Train)
+        Me.TabPage3.Controls.Add(Me.train_id_box)
+        Me.TabPage3.Controls.Add(Me.Label15)
+        Me.TabPage3.Controls.Add(Me.tbl_train)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(679, 391)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Train"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'btn_add
+        '
+        Me.btn_add.Font = New System.Drawing.Font("Roboto Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_add.Location = New System.Drawing.Point(551, 281)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(75, 57)
+        Me.btn_add.TabIndex = 37
+        Me.btn_add.Text = "ADD"
+        Me.btn_add.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Font = New System.Drawing.Font("Roboto Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_delete.Location = New System.Drawing.Point(551, 218)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(75, 57)
+        Me.btn_delete.TabIndex = 36
+        Me.btn_delete.Text = "DELETE"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_edit
+        '
+        Me.btn_edit.Location = New System.Drawing.Point(551, 155)
+        Me.btn_edit.Name = "btn_edit"
+        Me.btn_edit.Size = New System.Drawing.Size(75, 57)
+        Me.btn_edit.TabIndex = 35
+        Me.btn_edit.Text = "EDIT"
+        Me.btn_edit.UseVisualStyleBackColor = True
+        '
+        'name_box
+        '
+        Me.name_box.Location = New System.Drawing.Point(131, 67)
+        Me.name_box.Name = "name_box"
+        Me.name_box.Size = New System.Drawing.Size(111, 20)
+        Me.name_box.TabIndex = 30
+        '
+        'Train
+        '
+        Me.Train.AutoSize = True
+        Me.Train.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Train.Location = New System.Drawing.Point(39, 68)
+        Me.Train.Name = "Train"
+        Me.Train.Size = New System.Drawing.Size(74, 15)
+        Me.Train.TabIndex = 29
+        Me.Train.Text = "Train Name"
+        '
+        'train_id_box
+        '
+        Me.train_id_box.Location = New System.Drawing.Point(131, 20)
+        Me.train_id_box.Name = "train_id_box"
+        Me.train_id_box.Size = New System.Drawing.Size(111, 20)
+        Me.train_id_box.TabIndex = 28
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Roboto Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(39, 22)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(51, 15)
+        Me.Label15.TabIndex = 27
+        Me.Label15.Text = "Train ID"
+        '
+        'tbl_train
+        '
+        Me.tbl_train.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.tbl_train.Location = New System.Drawing.Point(42, 131)
+        Me.tbl_train.Name = "tbl_train"
+        Me.tbl_train.Size = New System.Drawing.Size(464, 239)
+        Me.tbl_train.TabIndex = 26
         '
         'crud
         '
@@ -226,6 +479,12 @@ Partial Class crud
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
         CType(Me.stationTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.inv_table, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        CType(Me.tbl_train, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -248,4 +507,27 @@ Partial Class crud
     Friend WithEvents nameBox As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents idBox As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents seat_id As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents identification As TextBox
+    Friend WithEvents passgr_name As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents tickets As TextBox
+    Friend WithEvents tkt_id As Label
+    Friend WithEvents invo_id As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents inv_table As DataGridView
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents btn_add As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_edit As Button
+    Friend WithEvents name_box As TextBox
+    Friend WithEvents Train As Label
+    Friend WithEvents train_id_box As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents tbl_train As DataGridView
 End Class
--- DeutscheBahn/crud.vb
+++ DeutscheBahn/crud.vb
@@ -13,6 +13,39 @@ Public Class crud
         ada.Fill(ds)
         stationTable.DataSource = ds.Tables(0)
 
+        ds.Dispose()
+        ada.Dispose()
+        conn.Close()
+    End Sub
+    Sub showInvTable()
+        Dim ada As New MySqlDataAdapter()
+        Dim ds As New DataSet
+
+        Call connect()
+
+        ada = New MySqlDataAdapter("SELECT * FROM invoice", conn)
+
+        ada.Fill(ds)
+        inv_table.DataSource = ds.Tables(0)
+
+        ds.Dispose()
+        ada.Dispose()
+        conn.Close()
+    End Sub
+    Sub showTrainTable()
+        Dim ada As New MySqlDataAdapter()
+        Dim ds As New DataSet
+
+        Call connect()
+
+        ada = New MySqlDataAdapter("SELECT * FROM series", conn)
+
+        ada.Fill(ds)
+        tbl_train.DataSource = ds.Tables(0)
+
+        ds.Dispose()
+        ada.Dispose()
+        conn.Close()
     End Sub
     Sub KodongkanData()
         idBox.Text = ""
@@ -20,10 +53,22 @@ Public Class crud
         routeidBox.Text = ""
         locidBox.Text = ""
     End Sub
+    Sub clearInvForms()
+        invo_id.Text = ""
+        passgr_name.Text = ""
+        tickets.Text = ""
+        identification.Text = ""
+        seat_id.Text = ""
+    End Sub
+    Sub clearTrainForms()
+        train_id_box.Text = ""
+        name_box.Text = ""
+    End Sub
 
     Private Sub crud_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Call showStatTable()
-
+        Call showInvTable()
+        Call showTrainTable()
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles addtodbBtn.Click
@@ -60,24 +105,25 @@ Public Class crud
         End If
     End Sub
 
-    Private Sub idBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles idBox.KeyPress
-        idBox.MaxLength = 3
-        If e.KeyChar = Chr(13) Then
-            Call connect()
-            cmd = New MySqlCommand("SELECT * FROM station WHERE id='" & idBox.Text & "'", conn)
-            rd = cmd.ExecuteReader
-            rd.Read()
-            If Not rd.HasRows Then
-                MsgBox("Station Not Found, make sure the ID is correct")
-                idBox.Focus()
-            Else
-                nameBox.Text = rd.Item("name")
-                locidBox.Text = rd.Item("loc_id")
-                routeidBox.Text = rd.Item("route_id")
-                nameBox.Focus()
-            End If
+
+    Private Sub idBox_Leave(sender As Object, e As EventArgs) Handles idBox.Leave
+        Call connect()
+        cmd = New MySqlCommand("SELECT * FROM station WHERE id='" & idBox.Text & "'", conn)
+        rd = cmd.ExecuteReader
+        rd.Read()
+        If Not rd.HasRows Then
+            MsgBox("ID Not Found, make sure the ID is correct")
+        Else
+            nameBox.Text = rd.Item("name")
+            locidBox.Text = rd.Item("loc_id")
+            routeidBox.Text = rd.Item("route_id")
+            nameBox.Focus()
         End If
+        rd.Close()
+        rd.Dispose()
+        conn.Close()
     End Sub
+
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Call connect()
@@ -87,5 +133,142 @@ Public Class crud
         MsgBox("Successfully Updated")
         Call showStatTable()
         Call KodongkanData()
+    End Sub
+
+    Sub updateTable(sqls As String)
+        Call connect()
+        Dim edit As String = sqls
+        cmd = New MySqlCommand(edit, conn)
+        cmd.ExecuteNonQuery()
+        MsgBox("Successfully Updated")
+
+        cmd.Dispose()
+        conn.Close()
+    End Sub
+    Sub deleteEntries(sqls As String)
+        Call connect()
+        Dim hapus As String = sqls
+        cmd = New MySqlCommand(hapus, conn)
+        cmd.ExecuteNonQuery()
+
+        cmd.Dispose()
+        conn.Close()
+    End Sub
+    Sub addEntries(sqls As String)
+        Call connect()
+
+        Dim insertSql As String = sqls
+
+        cmd = New MySqlCommand(insertSql, conn)
+        cmd.ExecuteNonQuery()
+
+        MsgBox("Input Data Berhasil")
+
+        cmd.Dispose()
+        conn.Close()
+    End Sub
+
+    'INVOICE TABLE UPDATE
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim sqls As String = "UPDATE invoice SET ticket_id='" & tickets.Text & "',name='" & passgr_name.Text & "',identification='" & identification.Text & "',seat_id='" & seat_id.Text & "' WHERE invoice_id='" & invo_id.Text & "'"
+        Call updateTable(sqls)
+        Call showInvTable()
+        Call clearInvForms()
+    End Sub
+
+    Private Sub invo_id_Leave(sender As Object, e As EventArgs) Handles invo_id.Leave
+        Call connect()
+        cmd = New MySqlCommand("SELECT * FROM invoice WHERE invoice_id='" & invo_id.Text & "'", conn)
+        rd = cmd.ExecuteReader
+        rd.Read()
+        If Not rd.HasRows Then
+            MsgBox("Invoice Not Found, make sure the ID is correct")
+        Else
+            tickets.Text = rd.Item("ticket_id")
+            passgr_name.Text = rd.Item("name")
+            identification.Text = rd.Item("identification")
+            seat_id.Text = rd.Item("seat_id")
+            tickets.Focus()
+        End If
+        rd.Close()
+        rd.Dispose()
+        conn.Close()
+    End Sub
+
+    'DELETE INVOICE ENTRIES
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        If invo_id.Text = "" Then
+            MsgBox("Please Fill Invoice ID")
+        Else
+            If MessageBox.Show("Are you sure to delete this data ?", "", MessageBoxButtons.YesNo) = System.Windows.Forms.DialogResult.Yes Then
+                Dim sqls As String = "DELETE FROM invoice  WHERE invoice_id='" & invo_id.Text & "'"
+                Call deleteEntries(sqls)
+                Call showInvTable()
+                Call clearInvForms()
+            End If
+        End If
+    End Sub
+
+    'INSERT INTO INVOICE TABLE
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If invo_id.Text = "" Or tickets.Text = "" Or passgr_name.Text = "" Or identification.Text = "" Or seat_id.Text = "" Then
+            MessageBox.Show("Please Fill All Forms")
+        Else
+
+            Dim insertSql As String = "INSERT INTO invoice values ('" & invo_id.Text & "','" & tickets.Text & "','" & passgr_name.Text & "','" & identification.Text & "','" & seat_id.Text & "','" & DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") & "')"
+            Call addEntries(insertSql)
+            Call showInvTable()
+            Call clearInvForms()
+
+        End If
+    End Sub
+
+    Private Sub train_id_box_Leave(sender As Object, e As EventArgs) Handles train_id_box.Leave
+        Call connect()
+        cmd = New MySqlCommand("SELECT * FROM series WHERE id='" & train_id_box.Text & "'", conn)
+        rd = cmd.ExecuteReader
+        rd.Read()
+        If Not rd.HasRows Then
+            MsgBox("Train Not Found, make sure the ID is correct")
+        Else
+            name_box.Text = rd.Item("name")
+            nameBox.Focus()
+        End If
+        rd.Close()
+        rd.Dispose()
+        conn.Close()
+    End Sub
+
+    'EDIT TRAIN ENTRIES
+    Private Sub btn_edit_Click(sender As Object, e As EventArgs) Handles btn_edit.Click
+        Dim sqls As String = "UPDATE series SET name='" & name_box.Text & "' WHERE id=" & train_id_box.Text
+        Call updateTable(sqls)
+        Call showTrainTable()
+        Call clearTrainForms()
+    End Sub
+
+    'DELETE TRAIN ENTRIES
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        If train_id_box.Text = "" Then
+            MsgBox("Please Fill Train ID")
+        Else
+            If MessageBox.Show("Are you sure to delete this data ?", "", MessageBoxButtons.YesNo) = System.Windows.Forms.DialogResult.Yes Then
+                Dim sqls As String = "DELETE FROM series WHERE id='" & train_id_box.Text & "'"
+                Call deleteEntries(sqls)
+                Call showTrainTable()
+                Call clearTrainForms()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_add_Click(sender As Object, e As EventArgs) Handles btn_add.Click
+        If train_id_box.Text = "" Or name_box.Text = "" Then
+            MessageBox.Show("Please Fill All Forms")
+        Else
+            Dim insertSql As String = "INSERT INTO series VALUES ('" & train_id_box.Text & "','" & name_box.Text & "')"
+            Call addEntries(insertSql)
+            Call showTrainTable()
+            Call clearTrainForms()
+        End If
     End Sub
 End Class
